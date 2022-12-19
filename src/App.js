79de0afdd66db599c5b56ad86fff1af7914904ba
Amit <109503020+amit95solanki@users.noreply.github.com>
2022-12-19 18:58:14 +0530

    routing change
    routing change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Hero from "./Component/Hero/Hero"
 import About from "./Component/About/About.js"
 import Qualification from "./Component/Qualification/Qualification.js";
 import Navbar from "./Component/Navbar.js";
-import {BrowserRouter, Routes,Route} from "react-router-dom"
+import {Routes,Route} from "react-router-dom"
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import Footer from "./Component/Footer/Footer.js";
@@ -13,7 +13,7 @@ import Footer from "./Component/Footer/Footer.js";
 function App() {
   return (
     <>
-    <BrowserRouter>
+    
     <Navbar/>
     <Routes>
     <Route path="/" element={<Hero/>}/>
@@ -22,7 +22,7 @@ function App() {
     <Route path="/qualification" element={<Qualification/>}/>
     </Routes>
     <Footer/>
-    </BrowserRouter>
+    
     </>
     
   );
